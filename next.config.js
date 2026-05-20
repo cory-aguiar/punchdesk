6fe49694d8f78bbd627e2e0e9ebc00933780adc8
cory-aguiar <cory@kiaahilo.org>
2026-05-19 22:21:10 -1000

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: blob:",
+              "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://fonts.googleapis.com",
+              "frame-src 'none'",
+            ].join('; ')
+          }
+        ]
+      }
+    ]
+  }
 }
 module.exports = nextConfig
